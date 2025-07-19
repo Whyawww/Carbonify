9a7167f4ab876ed381f30606c42d91c4d327873c
RAIHAN <raihan.alyzaky@gmail.com>
2025-07-20 00:20:29 +0700

    update page, tambah tantangan, panggil favico

---
+++ carbonify-app/src/lib/gamificationData.ts
@@ -0,0 +1,20 @@
+export const weeklyChallenges = [
+  {
+    id: 'wc01',
+    title: 'Tanpa Kantong Plastik',
+    description: 'Belanja menggunakan tas kain atau eco bag selama selama 7 hari!',
+    points: 50,
+  },
+  {
+    id: 'wc02',
+    title: 'Transportasi Hijau',
+    description: 'Gunakan transportasi umum atau sepeda selama 3 hari berturut-turut!',
+    points: 70,
+  },
+  {
+    id: 'wc03',
+    title: 'Hemat Energi',
+    description: 'Cabut charger, matikan lampu dan alat elektronik saat tidak digunakan selama 7 hari!',
+    points: 60,
+  },
+];
